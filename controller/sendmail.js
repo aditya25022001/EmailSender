be8b90000068e53755f855fd42b235ffcfb47881
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-05-07 15:01:50 +0530

    completed

--- controller/sendmail.js
+++ controller/sendmail.js
@@ -1,26 +1,47 @@
+
+//main package used for sending mails
 import nodemailer from 'nodemailer';
+
+//templating engine for sending mail and create beautiful templates
 import hbs from 'nodemailer-express-handlebars'
+
+//inbuilt module for resolving the path of the template files to look for sending emails
 import path from 'path'
+
+//package for reading the environment variables
 import dotenv from 'dotenv'
 
+//calling the config method to read the variables saved in the '.env file'
 dotenv.config()
 
+//main sending function
 const sendMail = (req,res) => {
     
+    //initializing __dirname since it is deprecate din ES6 so we need to do path.resolve()
     const __dirname = path.resolve()
     
+    //creating the transporter object which is the main instance to send emails
+    //passing two parameters in createTransport --> 
+    //service provider ( gmail in this case ) can be anything read here --> https://www.nodemailer.com/usage
+    //auth object taking in two keys user --> the user email who is sendng the email
+    //------------------------------ pass --> the password of email provided
     const transporter = nodemailer.createTransport({service: 'gmail',auth:{user:process.env.USER, pass:process.env.PASSWORD}})
-    
+
+    //setting the template engine for the email template using the path module
     transporter.use('compile',hbs({viewEngine:'nodemailer-express-handlebars', viewPath:path.join(__dirname,'/controller/views/')}))
     
-    let name;
+    //initializing some variables to provide a dynamic email
+    let name, emails;
 
+    //if request contains name query string then assign it to the name variable 
     if (req.query.name){
         name = req.query.name
     }
-
-    let emails
-
+    else{
+        name = 'Beginner'
+    }
+    
+    //if request contains emails query string then assign it to the emails variable ( can be more than one comma separated values ) 
     if(req.query.emails){
         emails = req.query.emails.split(',').toString()
     }
@@ -28,16 +49,32 @@ const sendMail = (req,res) => {
         emails = process.env.USER
     }
 
+    //the key part of the//setting the mailing options
+    //NOTE --> from and auth.user ( in createTransport ) should be same if using Gmail else it will give error
     const mailOptions = {
+        
+        //who is sending emails
         from : process.env.USER, 
+
+        //to whom is sending emails
         to: emails,
+
+        //subject of the email
         subject : 'Test boilerplate for sending emails',
+        
+        //use the handlebars file with name 'sample'
         template : 'sample',
+
+        //pass in the variable in the handlebars file
         context : {
             name:name
         }
     }
 
+    //calling the sendMail method on the transporter object created
+    //take sin two parameters mailOptions and a callback function
+    //the callback function takes two parameters ( error, info )
+    //see console logging each to know more
     transporter.sendMail(mailOptions,(err, info) => {
         if(err){
             console.log(err.message)
@@ -51,4 +88,5 @@ const sendMail = (req,res) => {
     })
 }
 
+//exporting the function sendMail
 export { sendMail } 
--- server.js
+++ server.js
@@ -1,21 +1,37 @@
+
+//importing express to create server  
 import express from 'express'
+
+//importing dotenv for reading the ev=nvironment variables
 import dotenv from 'dotenv'
+
+//importing the sendMail function
 import { sendMail } from './controller/sendmail.js'
 
+//initializing the config() method to read variables form .env file
 dotenv.config()
 
+//initializing the app
 const app = express()
 
-const PORT = process.env.PORT || 5002
-
+//not required but stating that use json data when any data passed from body
 app.use(express.json())
 
+//assigning port
+const PORT = process.env.PORT || 5002
+
+//root method 
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.send('Hello world! sending emails usning nodemailer, nodejs, express and handlebars')
 })
 
+//method to send emails
+//url structure : http://localhost:5002/sendmail/?emails=comma separated values&name=Single name of user
+//both are query strings so not necessary to provide
+//simply http://localhost:5002/sendmail will work
 app.post('/sendmail',(req,res)=>{
     sendMail(req,res)
 })
 
+//listening on port PORT
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
